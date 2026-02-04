06b1d59cc3b6dc878971660ccc2f7a9da075de97
saikumarseepana <saikumarseepana436@gmail.com>
2026-02-04 20:34:58 +0530

    feat: implemented ineractive loop in main.py

--- main.py
+++ main.py
@@ -17,12 +17,48 @@ logging.basicConfig(
 
 print(f"System Initialized. Logs are writing to: {log_file}")
 
-try:
-    alice = BankAccount(1000)
-    alice.deposit(3000)
-    print(f"Alice's balance: {alice.get_balance()}")
+# try:
+#     alice = BankAccount(1000)
+#     alice.deposit(3000)
+#     print(f"Alice's balance: {alice.get_balance()}")
 
-    alice.withdraw(5000) # We'll test the error
+#     alice.withdraw(5000) # We'll test the error
 
-except ValueError as e:
-    print(f"Error: {e}")
+# except ValueError as e:                                ## Hard coded test
+#     print(f"Error: {e}")
+
+def main():
+    print("---  FinCore Ledger System  ---")
+
+    account = BankAccount(balance=0)
+
+    while True:
+
+        print(f"\n Current Balance: {account.get_balance()}")
+        print("1. Deposit")
+        print("2. Withdraw")
+        print("3. Exit")
+
+        choice = input("Select an option (1 - 3): ")
+
+        try:
+            if choice == '1':
+                amount = float(input("Enter deposit amount: "))
+                account.deposit(amount)
+                print(f"Deposited: {amount}")
+
+            elif choice == '2':
+                amount = float(input("Enter withdrawal amount: "))
+                account.withdraw(amount)
+                print(f"Withdrew: {amount}")
+
+            elif choice == "3":
+                print("Saving data... Goodbye!")
+                break
+            else:
+                print("Invalid option. Please try again.")
+        except ValueError as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    main()
